13f935de1598df2cefd358c18c8d90699db29e26
Shubham Gupta <Anilkumargupta050368@gmail.com>
2026-02-27 01:38:30 +0530

    added DB

---
+++ src/db/dataBase.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+  try {
+    mongoose.connect(process.env.MONGO_URL);
+    console.log("Mongoose is connected");
+  } catch (error) {
+    console.error("Mongoose not connected");
+    process.exit(1);
+  }
+};
+
+export default connectDB;
