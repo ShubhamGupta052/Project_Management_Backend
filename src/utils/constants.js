72b36cf9e234436fb6f7130905c39edfe9ceab80
Shubham Gupta <Anilkumargupta050368@gmail.com>
2026-02-27 01:37:38 +0530

    Added Mongoose

---
+++ src/utils/constants.js
@@ -0,0 +1,15 @@
+export const UserRolesEnum = {
+  ADMIN: "admin",
+  PROJECT_ADMIN: "projectAdmin",
+  MEMBER: "member",
+};
+
+export const AvalibleUserRoles = Object.values(UserRolesEnum);
+
+export const ProjectStatusEnum = {
+  TODO: "todo",
+  IN_PROGRESS: "inProgress",
+  DONE: "done",
+};
+
+export const AvalibleProjectRoles = Object.values(ProjectStatusEnum);
