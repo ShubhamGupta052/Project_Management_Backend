8a7f1c12c7846456a14849d8f13ae614cce6bb4c
Shubham Gupta <Anilkumargupta050368@gmail.com>
2026-03-15 23:44:34 +0530

    added register user as well as login routes

---
+++ src/validators/index.js
@@ -0,0 +1,24 @@
+import { body } from "express-validator";
+
+const userRegisterValidator = () => {
+  return [
+    body("email")
+      .trim()
+      .notEmpty()
+      .withMessage("Email is requied")
+      .isEmail()
+      .withMessage("Email is invalid"),
+    body("username")
+      .trim()
+      .notEmpty()
+      .withMessage("Username is requied")
+      .isLowercase()
+      .withMessage("Username is must be Lowercase")
+      .isLength({ min: 3 })
+      .withMessage("Username must be at least 3 characters long"),
+    body("password").trim().notEmpty().withMessage("Password is requied"),
+    body("fullName").optional().trim(),
+  ];
+};
+
+export { userRegisterValidator };
